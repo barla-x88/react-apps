f96b808561b52c45d0f6610211f614d484f6559c
Barla Ankit <barla.x88@gmail.com>
2023-05-05 15:00:08 +0530

    04 - Render Objects present in the array
    In this example we render only one object at a time. When the button is
    clicked the next object in the array is rendered. As we already Know
    that state update do not happen immediately, we use useEffect hook to
    make this example work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { ToDoList } from "./components/ToDoList";
+import ObjectDisplay from "./components/ObjectDisplay";
 
 function App() {
   return (
     <div>
       <h1>React Hooks - useState</h1>
-      <ToDoList />
+      <ObjectDisplay />
     </div>
   );
 }
