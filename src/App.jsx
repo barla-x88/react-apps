6eee62d7db0eada432dd98dc30f8a01ebd945a9b
Barla Ankit <barla.x88@gmail.com>
2023-05-29 21:57:03 +0530

    22 - useRef Hook ( setting current value manually)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import GroceryBud from "./components/GroceryBud/GroceryBud";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
+  const [submitCount, setSubmitCount] = useState(0);
+
+  const refContainer = useRef(null);
+
+  console.log(refContainer); // {current: null} on initial render
+
+  //runs after the render
+  //this will run only once after the initial render because
+  //dependency array is empty
+  useEffect(() => {
+    //setting value manually
+    //this will not trigger rerender
+    refContainer.current = document.querySelector("#text");
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(refContainer.current.value);
+
+    //this will trigger rerender
+    setSubmitCount(submitCount + 1);
+  };
   return (
-    <>
-      <ToastContainer position="top-center" />
-      <GroceryBud toast={toast} />
-    </>
+    <div className="formContainer">
+      <form onSubmit={handleSubmit}>
+        <h1>Submit count : {submitCount}</h1>
+        <input type="text" id="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
   );
 };
 export default App;
